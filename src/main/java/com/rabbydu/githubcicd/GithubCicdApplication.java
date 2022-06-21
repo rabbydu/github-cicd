3050c730046310de57a19a02d089bf52e6cc27d8
Rabby Chowdhury <you@example.com>
2022-06-21 22:13:11 +0600

    first commit

---
+++ src/main/java/com/rabbydu/githubcicd/GithubCicdApplication.java
@@ -0,0 +1,13 @@
+package com.rabbydu.githubcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubCicdApplication.class, args);
+	}
+
+}
